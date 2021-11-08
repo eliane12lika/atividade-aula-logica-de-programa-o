e22e9c9c0d4dc9912971e388bac084888d26684c
eliane12lika <eliane.pimenntel@gmail.com>
2021-11-08 15:42:48 -0300

    exercicios logica de programacao

---
+++ exercicio1.js
@@ -0,0 +1,18 @@
+var num = 1;
+var numdois = 2;
+var numtres = 3;
+var numquatro = 4;
+var numcinco = 5;
+var total;
+total = num + numdois + numtres + numquatro + numcinco;
+console.log(total);
+console.log(total / 5);
+/*leia um número;
+leia numdois
+leia numtres
+leia numquatro
+leia numcinco*
+consolelog(total)/
+consolelog(total / 5)*/
+('soma das variáveis e média total')
+
---
+++ exercicio3.js
@@ -0,0 +1,76 @@
+numero = 0
+if(numero % 2 === 0) {
+    console.log("O número é par");
+}else{
+    console.log("O número é ímpar");
+}
+
+numero = 1
+if(numero % 2 === 0) {
+    console.log("O número é par");
+}else{
+    console.log("O número é ímpar");
+}
+
+numero = 2
+if(numero % 2 === 0) {
+    console.log("O número é par");
+}else{
+    console.log("O número é ímpar");
+}
+
+numero = 3
+if(numero % 2 === 0) {
+    console.log("O número é par");
+}else{
+    console.log("O número é ímpar");
+}
+
+numero = 4
+if(numero % 2 === 0) {
+    console.log("O número é par");
+}else{
+    console.log("O número é ímpar");
+}
+
+numero = 5
+if(numero % 2 === 0) {
+    console.log("O número é par");
+}else{
+    console.log("O número é ímpar");
+}
+
+numero = 6
+if(numero % 2 === 0) {
+    console.log("O número é par");
+}else{
+    console.log("O número é ímpar");
+}
+
+numero = 7
+if(numero % 2 === 0) {
+    console.log("O número é par");
+}else{
+    console.log("O número é ímpar");
+}
+
+numero = 8
+if(numero % 2 === 0) {
+    console.log("O número é par");
+}else{
+    console.log("O número é ímpar");
+}
+
+numero = 9
+if(numero % 2 === 0) {
+    console.log("O número é par");
+}else{
+    console.log("O número é ímpar");
+}
+
+numero = 10
+if(numero % 2 === 0) {
+    console.log("O número é par");
+}else{
+    console.log("O número é ímpar");
+}
